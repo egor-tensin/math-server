1a3bf05144711529a1bc8fa6cec3091d70c5b1ab
Egor Tensin <Egor.Tensin@gmail.com>
2019-12-22 12:17:01 +0300

    add licensing notes to source files

--- server/lexer/lexer.hpp
+++ server/lexer/lexer.hpp
@@ -1,3 +1,8 @@
+// Copyright (c) 2019 Egor Tensin <Egor.Tensin@gmail.com>
+// This file is part of the "math-server" project.
+// For details, see https://github.com/egor-tensin/math-server.
+// Distributed under the MIT License.
+
 #pragma once
 
 #include "input.hpp"
--- test/unit_tests/main.cpp
+++ test/unit_tests/main.cpp
@@ -1,2 +1,7 @@
+// Copyright (c) 2019 Egor Tensin <Egor.Tensin@gmail.com>
+// This file is part of the "math-server" project.
+// For details, see https://github.com/egor-tensin/math-server.
+// Distributed under the MIT License.
+
 #define BOOST_TEST_MODULE math_server tests
 #include <boost/test/included/unit_test.hpp>
